865cb8dfab71aaf5ae13a6a4b462edd2f0594ecd
Sok-ai <Sokol152433@gmail.com>
2025-04-08 21:42:35 +0300

    init

---
+++ src/main/java/com/example/axiomaticsTest/AxiomaticsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.axiomaticsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxiomaticsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxiomaticsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/axiomaticsTest/AxiomaticsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.axiomaticsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxiomaticsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
