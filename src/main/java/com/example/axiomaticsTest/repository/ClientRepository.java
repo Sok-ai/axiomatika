8ba0764df20ad6179209de6c81884e539f1e8b1d
Sok-ai <Sokol152433@gmail.com>
2025-04-09 12:24:54 +0300

    Add: implement client search methods in ClientRepository
    - Add findByPhone using Spring Data JPA
    - Add findByPassport using Spring Data JPA
    - Add findByFullName using Spring Data JPA

--- src/main/java/com/example/axiomaticsTest/repository/ClientRepository.java
+++ src/main/java/com/example/axiomaticsTest/repository/ClientRepository.java
@@ -2,6 +2,15 @@ package com.example.axiomaticsTest.repository;
 
 import com.example.axiomaticsTest.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByPhone(String phone);
+
+    Optional<Client> findByPassport(String passport);
+
+    Optional<Client> findByFullName(String fullName);
 }
