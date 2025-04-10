cf4b9d732ca9e02fb2aa0ccd6cf365a8d65ed46b
Sok-ai <Sokol152433@gmail.com>
2025-04-10 10:45:23 +0300

    Add: implement LoanApplicationService for loan management
    - Add getAllApplications to retrieve all loan applications
    - Add getApprovedApplications to filter approved loans
    - Add createApplication to save new loan application with client

---
+++ src/main/java/com/example/axiomaticsTest/service/LoanApplicationService.java
@@ -0,0 +1,37 @@
+package com.example.axiomaticsTest.service;
+
+import com.example.axiomaticsTest.model.Client;
+import com.example.axiomaticsTest.model.LoanApplication;
+import com.example.axiomaticsTest.repository.ClientRepository;
+import com.example.axiomaticsTest.repository.LoanApplicationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoanApplicationService {
+    private final LoanApplicationRepository loanApplicationRepository;
+    private final ClientRepository clientRepository;
+
+    public LoanApplicationService(LoanApplicationRepository loanApplicationRepository, ClientRepository clientRepository) {
+        this.loanApplicationRepository = loanApplicationRepository;
+        this.clientRepository = clientRepository;
+    }
+
+    public List<LoanApplication> getAllApplications() {
+        return loanApplicationRepository.findAll();
+    }
+
+    public List<LoanApplication> getApprovedApplications() {
+        return loanApplicationRepository.findAll().stream().filter(loan -> loan.getDecision() != null && loan.getDecision().getApproved()).toList();
+    }
+
+    public void createApplication(LoanApplication application) {
+        Client savedClient = clientRepository.save(application.getClient());
+        LoanApplication loanApplication = new LoanApplication();
+        loanApplication.setClient(savedClient);
+        loanApplication.setDesiredAmount(application.getDesiredAmount());
+        loanApplication.setCreditPurpose(application.getCreditPurpose());
+        loanApplicationRepository.save(loanApplication);
+    }
+}
