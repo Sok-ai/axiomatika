099dc78819340f03aa18a391b9ae07c03376088c
Sok-ai <Sokol152433@gmail.com>
2025-04-11 10:38:22 +0300

    Refactor: an unused findByApplication method in the LoanContractRepository

--- src/main/java/com/example/axiomaticsTest/repository/LoanContractRepository.java
+++ src/main/java/com/example/axiomaticsTest/repository/LoanContractRepository.java
@@ -1,11 +1,7 @@
 package com.example.axiomaticsTest.repository;
 
-import com.example.axiomaticsTest.models.LoanApplication;
 import com.example.axiomaticsTest.models.LoanContract;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface LoanContractRepository extends JpaRepository<LoanContract, Long> {
-    Optional<LoanContract> findByApplication(LoanApplication application);
 }
