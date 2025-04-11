5f6c77cb6016d39e33db98212350d0da8784f3db
Sok-ai <Sokol152433@gmail.com>
2025-04-11 09:35:25 +0300

    Add: methods in the LoanApplicationController
    - Add signContract to sign a contract
    - Add showContracts to display a list of signed contracts

--- src/main/java/com/example/axiomaticsTest/controller/LoanApplicationController.java
+++ src/main/java/com/example/axiomaticsTest/controller/LoanApplicationController.java
@@ -1,20 +1,27 @@
 package com.example.axiomaticsTest.controller;
 
-import com.example.axiomaticsTest.model.Client;
-import com.example.axiomaticsTest.model.LoanApplication;
+import com.example.axiomaticsTest.models.Client;
+import com.example.axiomaticsTest.models.LoanApplication;
+import com.example.axiomaticsTest.models.LoanContract;
 import com.example.axiomaticsTest.service.LoanApplicationService;
+import com.example.axiomaticsTest.service.LoanContractService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class LoanApplicationController {
     private final LoanApplicationService loanApplicationService;
+    private final LoanContractService loanContractService;
 
-    public LoanApplicationController(LoanApplicationService loanApplicationService) {
+    public LoanApplicationController(LoanApplicationService loanApplicationService, LoanContractService loanContractService) {
         this.loanApplicationService = loanApplicationService;
+        this.loanContractService = loanContractService;
     }
 
     @GetMapping("applications/new")
@@ -33,13 +40,26 @@ public class LoanApplicationController {
 
     @GetMapping(path = "applications")
     public String showApplications(Model model) {
-        model.addAttribute("loads", loanApplicationService.getAllApplications());
+        model.addAttribute("loans", loanApplicationService.getAllApplications());
         return "applications/showApplications";
     }
 
     @GetMapping(path = "applications/approved")
     public String showApprovedApplications(Model model) {
-        model.addAttribute("loads", loanApplicationService.getApprovedApplications());
+        model.addAttribute("loans", loanApplicationService.getApprovedApplications());
         return "applications/approved";
+    }
+
+    @PostMapping("applications/{id}/sign")
+    public String signContract(@PathVariable Long id) {
+        loanContractService.signContract(id);
+        return "redirect:/applications/approved";
+    }
+
+    @GetMapping(path = "applications/contracts")
+    public String showContracts(Model model) {
+        List<LoanContract> signedContracts = loanContractService.getApprovedContracts();
+        model.addAttribute("contracts", signedContracts);
+        return "contracts/signed-list";
     }
 }
